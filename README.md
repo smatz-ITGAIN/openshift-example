561052f7bd07354882d37e41ce557bb1038e486b
Stefan Matz <stefan.matz@itgain.de>
2023-01-17 17:02:15 +0100

    Add hello route

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # openshift-example
 
 Create Spring Boot Projekt with https://start.spring.io/
+
+# Build and run java app
+
+## Build
+
+```bash
+./gradlew clean build
+```
+
+## Run
+
+```bash
+./gradlew bootRun
+```
--- src/main/java/de/itgain/openshift/OpenshiftApplication.java
+++ src/main/java/de/itgain/openshift/OpenshiftApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OpenshiftApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Server is running on port 8080. Visit http://localhost:8080");
 		SpringApplication.run(OpenshiftApplication.class, args);
 	}
 
